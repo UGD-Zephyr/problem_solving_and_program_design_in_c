aebb302f586df9931ae6a6364f1891af3a7b0e26
Zephyr <ugd.zephyr@gmail.com>
2023-02-14 13:35:48 +0100

    committing files

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+/* Programmer: Per Stoor
+ * Date: 2023-02-14
+ * Last changed: 2023-02-14
+ * Type of program: 
+ */
+
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main(int argc, char *argv[]){ 
+
+    printf("Hello World!\n}");
+
+return 0;
+} 
